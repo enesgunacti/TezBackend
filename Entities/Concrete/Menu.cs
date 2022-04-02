8c60eada8d025935215fca7395b83d2c6f3f35d2
enesg <gnctenes@gmail.com>
2022-04-02 04:45:22 +0300

    Add project files.

---
+++ Entities/Concrete/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string MenuUrl { get; set; }
+    }
+}
